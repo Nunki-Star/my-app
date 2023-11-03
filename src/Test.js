3c3d39fd95f4aad607a035244e585885c71040b3
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-11-03 15:29:19 +0300

    react folder created with 2 comonents

---
+++ src/Test.js
@@ -0,0 +1,9 @@
+function Test(){
+    return(
+        <>
+            <h1>My new Test component</h1>
+        </>
+    )
+}
+
+export default Test;
---
+++ src/Test2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Test2 extends React.Component{
+    render(){
+       return(
+        <h2>My Test2</h2>
+       )
+    }
+}
+
+export default Test2;
