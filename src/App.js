6756475a4879f86f15e730a5b3f7fb6f6bf26e05
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-11-03 19:01:52 +0300

    test1, test2 header created

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Test from './Test';
 import Test2 from './Test2';
-import Test3 from './Test3';
-import MyTest4 from './MyTest4';
-import Test5 from './Test5'
+import Header from './Header/Header'
 
 function App() {
   return (
     <>
-      <Test></Test>
-      <Test2></Test2>
-      <Test3></Test3>
-      <MyTest4></MyTest4>
-      <Test5></Test5>
+    <Header/>
+    <Test />
+    <Test2 />
     </>
   );
 }
